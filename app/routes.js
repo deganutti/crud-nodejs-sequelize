c6887bfeeb93d6e5727d74b4029c59caac4372e4
Luiz Gabriel Deganutti <deganutti@outlook.com.br>
2022-01-02 19:58:21 -0300

    Ajuste das rotas melhor separação

--- app/routes.js
+++ app/routes.js
@@ -1,35 +1,33 @@
-const express = require('express');
-const NivelAcessoController = require('./controller/NivelAcessoController');
-const UsuariosController = require('./controller/UsuariosController');
+const express = require("express");
+const NivelAcessoController = require("./controller/NivelAcessoController");
+const UsuariosController = require("./controller/UsuariosController");
 
 const routes = express.Router();
 
 /**
  * Criando as rotas de dados
  */
-
-//rota principal 
-routes.get('/',(req, res) => {
-    return res.json({
-        "Autor":"Luiz Gabriel Deganutti",
-        "Versão":"1.0.0",
-        "Aplicação":"Crud NodeJs com Sequelize e Mysql",
-        "Contato":{  
-            "E-Mail":"deganutti@outlook.com",
-        }
-    });
+//rota principal
+routes.get("/", (req, res) => {
+  return res.json({
+    Autor: "Luiz Gabriel Deganutti",
+    Versão: "1.0.0",
+    Aplicação: "Crud NodeJs com Sequelize e Mysql",
+    Contato: {
+      "E-Mail": "deganutti@outlook.com",
+    },
+  });
 });
 //rota de nivel acesso
-routes.get('/nivelacesso', NivelAcessoController.index);
-routes.post('/nivelacesso', NivelAcessoController.store);
-routes.put('/nivelacesso/:id', NivelAcessoController.post);
-routes.delete('/nivelacesso/:id', NivelAcessoController.del);
+routes.get("/nivelacesso", NivelAcessoController.index);
+routes.post("/nivelacesso", NivelAcessoController.store);
+routes.put("/nivelacesso/:id", NivelAcessoController.post);
+routes.delete("/nivelacesso/:id", NivelAcessoController.del);
 
 //rota de usuarios
-routes.get('/usuarios',UsuariosController.index);
-routes.post('/usuarios',UsuariosController.store);
-routes.put('/usuarios/:id',UsuariosController.post);
-routes.delete('/usuarios/:id',UsuariosController.del);
-
+routes.get("/usuarios", UsuariosController.index);
+routes.post("/usuarios", UsuariosController.store);
+routes.put("/usuarios/:id", UsuariosController.post);
+routes.delete("/usuarios/:id", UsuariosController.del);
 
 module.exports = routes;
